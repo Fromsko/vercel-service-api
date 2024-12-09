afe5270b4b02af234338f2d34d27e14d6ba5ac68
fromsko <1614355756@qq.com>
2024-12-10 02:07:38 +0800

    fix: 修改页面信息

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 "use client";
-import { Button } from "@rtdui/core";
 import Auth from "./views/auth";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Button>Login</Button>
-
-    //     <Auth/>
-    //   </div>
-    // </main>
     <Auth/>
   );
 }
