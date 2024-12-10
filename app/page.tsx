646a36770b3de8e9f888b78463f042802aa5f51d
fromsko <1614355756@qq.com>
2024-12-11 00:36:52 +0800

    feat: change something.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
-"use client";
+'use client'
 // import Auth from "./views/auth";
+import { Button, Card, Form, Input } from 'antd';
 
 export default function Home() {
+  const onFinish = (values: any) => {
+    console.log('Received values: ', values);
+  };
+
   return (
-    // <Auth/>
-    <div className="btn btn-success">
-      Hello
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Card className="w-96 p-2 shadow-lg" title="登录" bordered={false}>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          layout="vertical"
+        >
+          <Form.Item
+            name="username"
+            label="用户名"
+            rules={[{ required: true, message: '请输入用户名' }]}
+          >
+            <Input placeholder="请输入用户名" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="密码"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input.Password placeholder="请输入密码" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 focus:bg-blue-700"
+            >
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 }
